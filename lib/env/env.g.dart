b5191fbb01526c3fa155e9523cfe6c04b1267c7f
JundaJagad <121542411+JundaJagad@users.noreply.github.com>
2024-07-08 10:01:46 +0700

    first commit

---
+++ lib/env/env.g.dart
@@ -0,0 +1,7 @@
+part of 'env.dart';
+
+final class _Env {
+  static const String apiKey = 'https://miniprojapi-vercel.vercel.app/item';
+  static const String apiKey2 = 'https://miniprojapi-vercel.vercel.app/alamat';
+  static const String apiGemini = 'AIzaSyCwaOlOzGlRIVk_3324bGO0W_BNhPWO83k';
+}
